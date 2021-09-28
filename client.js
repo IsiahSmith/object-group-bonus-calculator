ed73ec9b604d2e158a289b2408a3aef92b312d73
Isiah Smith <isiahthomassmith@gmail.com>
2021-09-28 16:44:16 -0500

    Worked with Hamsa and Rob, didn't complete

--- client.js
+++ client.js
@@ -39,4 +39,78 @@ const employees = [
 // This is not a race. Everyone on your team should understand what is happening.
 // Ask questions when you don't.
 
-console.log( employees );
+function bonusLogic(worker) {
+  for (let i = 0; i < employees.length; i++) {
+    let finalObject = {
+      name: worker,
+      bonusPercentage: 'null',
+      bonusCompensation: 'null',
+      totalBonus: 'null'
+    }
+    if (worker === employees[i].name)
+
+
+
+
+
+
+      return finalObject
+  }
+}; //makes the new object
+console.log(bonusLogic('Scout'));
+console.log(bonusLogic('Mayella'));
+console.log(bonusLogic('Jem'));
+console.log(employees);
+
+
+function totalBonus(array) {
+  let noBonus = 0;
+  let addedBonus = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (employees[i].reviewRating === 3) {
+      addedBonus = employees[i].annualSalary * .04;
+    } else if (employees[i].reviewRating === 4) {
+      addedBonus = employees[i].annualSalary * .06;
+    } else if (employees[i].reviewRating === 5) {
+      addedBonus = employees[i].annualSalary * .1;
+    } else {
+      noBonus = employees[i].annualSalary / 1;
+    }
+      console.log((employees[i].annualSalary / 1) + addedBonus);
+      console.log(noBonus)
+    }
+  } //will compute the final employee total bonus INCOMPLETE
+
+  totalBonus(employees);
+
+  function totalComp(array){
+    let addedBonus = 0;
+    for (let i = 0; i < array.length; i++) {
+      if (employees[i].employeeNumber.length === 4){
+        addedBonus = employees[i].annualSalary * .05;
+      }else{
+        addedBonus = employees[i].annualSalary * 0;
+      }
+    console.log(addedBonus)
+  }
+  }
+  totalComp(employees);
+  function bonusCalc(array) {
+    let addedBonus = 0;
+    for (let i = 0; i < array.length; i++) {
+      if (employees[i].reviewRating === 3) {
+        addedBonus = employees[i].annualSalary * .04;
+      } else if (employees[i].reviewRating === 4) {
+        addedBonus = employees[i].annualSalary * .06;
+      } else if (employees[i].reviewRating === 5) {
+        addedBonus = employees[i].annualSalary * .1;
+      } else if (employees[i].reviewRating < 3) {
+        addedBonus = employees[i].annualSalary * 0;
+      }
+      console.log(addedBonus);
+    }
+  }
+
+  bonusCalc(employees);
+
+
